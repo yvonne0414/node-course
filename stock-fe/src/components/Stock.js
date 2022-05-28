3cab5ab3a6fe572dd2a1ef2d2d314833a43f7f0b
yvonne0414 <yvonne9456@gmail.com>
2022-05-28 16:34:43 +0800

    upd stock-fe

--- stock-fe/src/components/Stock.js
+++ stock-fe/src/components/Stock.js
@@ -1,16 +1,30 @@
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const Stock = () => {
+  const [stocks, setStocks] = useState([]);
+  useEffect(() => {
+    let getStocks = async () => {
+      let res = await axios.get('http://localhost:3001/stocks');
+      setStocks(res.data);
+    };
+    getStocks();
+  }, [stocks]);
   return (
     <div>
       <h2 className="ml-7 mt-6 text-xl text-gray-600">股票代碼</h2>
 
-      <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
-        <Link to={`/stock/1234`}>
-          <h2 className="text-2xl font-bold mb-2 text-gray-800">測試股票代號</h2>
-          <p className="text-gray-700">測試股票名稱</p>
-        </Link>
-      </div>
+      {stocks.map((stock) => {
+        return (
+          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+            <Link to={`/stock/${stock.id}`}>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">{stock.id}</h2>
+              <p className="text-gray-700">{stock.name}</p>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
