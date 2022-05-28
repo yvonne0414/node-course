32d6edec6bd7f1c790977a2d838f83f69e64da2c
yvonne0414 <yvonne9456@gmail.com>
2022-05-29 00:28:47 +0800

    upd fe stockDetail

--- stock-fe/src/components/Stock.js
+++ stock-fe/src/components/Stock.js
@@ -17,7 +17,7 @@ const Stock = () => {
 
       {stocks.map((stock) => {
         return (
-          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+          <div key={stock.id} className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
             <Link to={`/stock/${stock.id}`}>
               <h2 className="text-2xl font-bold mb-2 text-gray-800">{stock.id}</h2>
               <p className="text-gray-700">{stock.name}</p>
--- stock-fe/src/components/StockDetails.js
+++ stock-fe/src/components/StockDetails.js
@@ -1,17 +1,36 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 const StockDetails = () => {
+  const [stockDetails, setStockDetails] = useState([]);
+  const stockId = useParams().stockId;
+  // console.log(stockId);
+  useEffect(() => {
+    let getStocks = async () => {
+      let res = await axios.get(`http://localhost:3001/stockDetails/${stockId}`);
+      setStockDetails(res.data);
+    };
+    getStocks();
+  }, [stockDetails]);
   return (
     <div>
-      <div className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6">
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">日期：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交金額：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交股數：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">開盤價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">收盤價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">漲跌價差：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">最高價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">最低價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交筆數：</h2>
-      </div>
+      <div className="m-6 text-right">共{stockDetails.length}筆</div>
+      {stockDetails.map((detail) => {
+        return (
+          <div key={detail.date} className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6">
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">日期：{detail.date}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">成交金額：{detail.delta_price}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">成交股數：{detail.volume}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">開盤價：{detail.open_price}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">收盤價：{detail.close_price}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">漲跌價差：{detail.transactions}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">最高價：{detail.high_price}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">最低價：{detail.low_price}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">成交筆數：{detail.amount}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 };
