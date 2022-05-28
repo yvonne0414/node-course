02db7d42fed05fb794d2ac465ec6e65120dcb50f
yvonne0414 <yvonne9456@gmail.com>
2022-05-28 15:53:59 +0800

    add stock-fe

---
+++ stock-fe/src/components/About.js
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div className="m-7">
+      <h2 className="m-7 text-2xl text-gray-600">這裡是魚股市</h2>
+
+      <>
+        <h3>Hi, 小明</h3>
+        <img />
+      </>
+    </div>
+  );
+};
+
+export default About;
