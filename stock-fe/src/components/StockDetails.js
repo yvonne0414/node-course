210babd78847aa3bf0181aafe43eb126b37faef3
yvonne0414 <yvonne9456@gmail.com>
2022-05-29 14:59:10 +0800

    upd fe-pagination

--- stock-fe/src/components/StockDetails.js
+++ stock-fe/src/components/StockDetails.js
@@ -1,22 +1,61 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
 
 const StockDetails = () => {
   const [stockDetails, setStockDetails] = useState([]);
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [pageto, setPageto] = useState(1);
   const stockId = useParams().stockId;
   // console.log('stockId', stockId);
   useEffect(() => {
     let getStocks = async () => {
-      let res = await axios.get(`http://localhost:3001/stockDetails/${stockId}`);
+      let res = await axios.get(`http://localhost:3001/stockDetails/${stockId}`, {
+        params: {
+          page: pageto,
+        },
+      });
       setStockDetails(res.data.data);
+      setPage(res.data.pagination.page);
+      setLastPage(res.data.pagination.lastPage);
+      setTotal(res.data.pagination.total);
+      // console.log(res.data);
     };
     getStocks();
-  }, [stockId]);
-  // console.log('stockDetails', stockDetails);
+  }, [stockId, pageto]);
+  // console.log('page', page);
+  const getPages = () => {
+    let pages = [];
+    for (let i = 1; i <= lastPage; i++) {
+      if (i == page) {
+        pages.push(
+          <li className="pageItem active" key={i}>
+            {i}
+          </li>
+        );
+      } else {
+        pages.push(
+          <li className="pageItem" key={i} id={i} onClick={(e) => setPageto(e.target.id)}>
+            {i}
+          </li>
+        );
+      }
+    }
+    return pages;
+  };
   return (
     <div>
-      <div className="m-6 text-right">1/6頁，共{stockDetails.length}筆</div>
+      <div className="flex justify-between m-6">
+        {/* <input type="number" name="" id="" value={pageto} onChange={(e) => setPageto(e.target.value)} /> */}
+        <ul className="flex">{getPages()}</ul>
+        <div className="text-right">
+          {page}/{lastPage}頁，共{total}筆
+        </div>
+      </div>
+
       {stockDetails.map((detail) => {
         return (
           <div key={detail.date} className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6">
